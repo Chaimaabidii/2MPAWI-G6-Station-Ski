8e8cb307916cf410d8d1a61741f780fc8c5df4da
mayssaarfaoui <147447250+mayssaarfaoui@users.noreply.github.com>
2025-11-30 16:41:08 +0100

    Add AWS resources for EKS cluster and security groups

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,93 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:34bbd7b24160f92d6c29003080fb167c090d913dbd46eb973658eef0986112a0
-size 2009
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = var.vpc_cidr  # Utilisation de la variable pour le CIDR
+}
+
+resource "aws_security_group" "eks_cluster_sg" {
+  name        = "eks-cluster-sg-${var.cluster_name}"
+  description = "Security group for EKS cluster ${var.cluster_name}"
+  vpc_id      = var.vpc_id  # Utilisation de la variable pour l'ID du VPC
+
+  ingress {
+    from_port   = 8083
+    to_port     = 8083
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 30000
+    to_port     = 30000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-cluster-sg-${var.cluster_name}"
+  }
+}
+
+resource "aws_security_group" "eks_worker_sg" {
+  name        = "eks-worker-sg-${var.cluster_name}"
+  description = "Security group for EKS worker nodes ${var.cluster_name}"
+  vpc_id      = var.vpc_id  # Utilisation de la variable pour l'ID du VPC
+
+  ingress {
+    from_port   = 8083
+    to_port     = 8083
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 30000
+    to_port     = 30000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-worker-sg-${var.cluster_name}"
+  }
+}
+
+resource "aws_eks_cluster" "my_cluster" {
+  name     = var.cluster_name
+  role_arn = var.role_arn
+  version  = "1.30"
+
+  vpc_config {
+    subnet_ids         = var.subnet_ids
+    security_group_ids = [aws_security_group.eks_cluster_sg.id]
+  }
+}
+
+resource "aws_eks_node_group" "my_node_group" {
+  cluster_name    = aws_eks_cluster.my_cluster.name
+  node_group_name = "noeud1"
+  node_role_arn   = var.role_arn
+  subnet_ids      = var.subnet_ids
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+}
