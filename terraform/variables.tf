d511294588be69487db6a54c1d4ebab5bde489c0
mayssaarfaoui <147447250+mayssaarfaoui@users.noreply.github.com>
2025-11-30 19:52:14 +0100

    Remove descriptions from Terraform variables
    Removed descriptions from variables in variables.tf.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,16 @@
 variable "aws_region" {
-  description = "La région AWS"
-  type        = string
-  default     = "us-east-1"
+  type    = string
+  default = "us-east-1"
 }
 
 variable "cluster_name" {
-  description = "Nom du cluster EKS"
-  type        = string
-  default     = "mykubernetes"
+  type    = string
+  default = "mykubernetes"
 }
 
 variable "subnet_ids" {
-  description = "IDs des sous-réseaux EKS"
-  type        = list(string)
-  default     = [
+  type = list(string)
+  default = [
     "subnet-022338cd7540b4755",
     "subnet-03cbd72fbbfc663f1",
     "subnet-0bb0d896bd2043f48"
@@ -21,29 +18,20 @@ variable "subnet_ids" {
 }
 
 variable "role_arn" {
-  description = "ARN du rôle IAM pour EKS"
-  type        = string
-  default     = "arn:aws:iam::093685644326:role/LabRole"
+  type    = string
+  default = "arn:aws:iam::093685644326:role/LabRole"
 }
 
 variable "vpc_id" {
-  description = "ID du VPC"
-  type        = string
-  default     = "vpc-08161a2ce2c9925c1"
+  type    = string
+  default = "vpc-08161a2ce2c9925c1"
 }
 
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
+# ✅ SG EXISTANTS
 variable "eks_cluster_sg_id" {
-  description = "Security Group existant du cluster EKS"
-  type        = string
+  type = string
 }
 
 variable "eks_worker_sg_id" {
-  description = "Security Group existant des workers EKS"
-  type        = string
+  type = string
 }
