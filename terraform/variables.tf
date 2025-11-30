799efc4c9264356f91bc94110327a20941a75983
mayssaarfaoui <147447250+mayssaarfaoui@users.noreply.github.com>
2025-11-30 20:15:41 +0100

    Refactor AWS variables and add security group IDs
    Updated AWS variable defaults and added security group variables.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,40 +1,37 @@
 variable "aws_region" {
-  description = "La région AWS"
-  type        = string
-  default     = "us-east-1"
+  type    = string
+  default = "us-east-1"
 }
 
 variable "cluster_name" {
-  description = "Nom du cluster EKS"
-  type        = string
-  default     = "mykubernetes"
+  type    = string
+  default = "mykubernetes"
 }
 
 variable "subnet_ids" {
-  description = "IDs des sous-réseaux EKS"
-  type        = list(string)
-  default     = [
-    "subnet-0d6e3e6e1b76c3484",  # us-east-1a
-    "subnet-0a085f17f77568373",  # us-east-1b
-    "subnet-0679e41751e67f56a"   # us-east-1c
+  type = list(string)
+  default = [
+    "subnet-022338cd7540b4755",
+    "subnet-03cbd72fbbfc663f1",
+    "subnet-0bb0d896bd2043f48"
   ]
 }
 
 variable "role_arn" {
-  description = "ARN du rôle IAM pour EKS"
-  type        = string
-  default     = "arn:aws:iam::917654454800:role/LabRole"
+  type    = string
+  default = "arn:aws:iam::093685644326:role/LabRole"
 }
 
 variable "vpc_id" {
-  description = "ID du VPC"
-  type        = string
-  default     = "vpc-0fbaf3350fd25cf98"
+  type    = string
+  default = "vpc-08161a2ce2c9925c1"
 }
 
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
+# ✅ SG EXISTANTS
+variable "eks_cluster_sg_id" {
+  type = string
 }
 
+variable "eks_worker_sg_id" {
+  type = string
+}
