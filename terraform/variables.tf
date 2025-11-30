abc3fa93a1f7673fc84c12bfa3ac6083a3c738ef
mayssaarfaoui <147447250+mayssaarfaoui@users.noreply.github.com>
2025-11-30 20:29:03 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,20 +11,20 @@ variable "cluster_name" {
 variable "subnet_ids" {
   type = list(string)
   default = [
-    "subnet-022338cd7540b4755",
-    "subnet-03cbd72fbbfc663f1",
-    "subnet-0bb0d896bd2043f48"
+    "subnet-0679e41751e67f56a",
+    "subnet-03f7b4862fc55b6d4",
+    "subnet-0567d2939279428b3"
   ]
 }
 
 variable "role_arn" {
   type    = string
-  default = "arn:aws:iam::093685644326:role/LabRole"
+  default = "arn:aws:iam::917654454800:role/LabRole"
 }
 
 variable "vpc_id" {
   type    = string
-  default = "vpc-08161a2ce2c9925c1"
+  default = "vpc-0fbaf3350fd25cf981"
 }
 
 # ✅ SG EXISTANTS
