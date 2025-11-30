4a2c4f17837b48bb5ca2e620fbdad98fff1f1e4e
arfaouimayssa10@gmail.com <arfaouimayssa10@gmail.com>
2025-11-30 14:29:10 +0100

    Add terraform binaries using Git LFS

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:379f6e66f28b96478944c12d89fa37e89affd0d6a51f0c21ddb37081b92e58e1
+oid sha256:b54b5cf8ab8cde2d7fbf9273e6b0aac91799f84c2e032efd8491db01639282c6
 size 872
