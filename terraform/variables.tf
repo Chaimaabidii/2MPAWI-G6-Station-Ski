6551b3310bf7e96045bb1b45221b014531ead55a
mayssaarfaoui <147447250+mayssaarfaoui@users.noreply.github.com>
2025-12-01 10:39:15 +0100

    Update subnet IDs and role ARN in variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,22 +14,22 @@ variable "subnet_ids" {
   description = "IDs des sous-réseaux EKS"
   type        = list(string)
   default     = [
-    "subnet-0cd619f45e4754614",  # us-east-1a
-    "subnet-08806cd912ea9d7fd",  # us-east-1b
-    "subnet-04d7f2e10287f6830"   # us-east-1c
+    "subnet-004159f665fb20e5d",  # us-east-1a
+    "subnet-0742f1238ac9dd375",  # us-east-1b
+    "subnet-08d2543ad620d23e9"   # us-east-1c
   ]
 }
 
 variable "role_arn" {
   description = "ARN du rôle IAM pour EKS"
   type        = string
-  default     = "arn:aws:iam::093685644326:role/LabRole"
+  default     = "arn:aws:iam::939147848217:role/LabRole"
 }
 
 variable "vpc_id" {
   description = "ID du VPC"
   type        = string
-  default     = "vpc-078f190a4d0154205"
+  default     = "vpc-00be567b9a7f2b34e"
 }
 
 variable "vpc_cidr" {
