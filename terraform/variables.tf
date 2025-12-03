17ec007e74c25e1019c85423151396309298726b
arfaouimayssa10@gmail.com <arfaouimayssa10@gmail.com>
2025-12-03 16:42:15 +0100

    variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0b762e8cf0656bb4f207963b5a7d3a2f562fb6ceae7deee0f6128d69e64075d
+oid sha256:b69437bc229cc3e244cac64bb17865a6152b284eb65c81629064fe839df1f6c0
 size 872
